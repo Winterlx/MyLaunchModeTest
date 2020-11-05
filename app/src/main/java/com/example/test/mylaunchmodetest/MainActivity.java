6f068f9997f3d915e316880b043fb019374f1b75
李璇 <lixuan@lixuandeMBP.lan>
2020-11-05 20:02:08 +0800

    init

---
+++ app/src/main/java/com/example/test/mylaunchmodetest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.test.mylaunchmodetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void btn1Click(View view) {
+    }
+
+    public void btn2Click(View view) {
+    }
+
+    public void btn3Click(View view) {
+    }
+
+    public void btn4Click(View view) {
+    }
+
+
+}
